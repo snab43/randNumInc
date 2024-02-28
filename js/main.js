c646f5dbf42a2544e0524259ab7165ba185e48a4
snaber <94073283+snab43@users.noreply.github.com>
2024-02-28 18:54:23 -0500

    Added additional numbers and styles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ const LAST_RESULTS_LENGTH = 100;
 let stats = {
 	numbers: 0,
 	primeNumbers: 0,
+	tenNumbers: 0,
+
+	totalNumbers: 0,
+	totalTime: 0,
 	
 	clickMin: 1,
 	clickMax: 5,
@@ -18,7 +22,15 @@ let stats = {
 	autoclickerMax: 10,
 	autoclickerMinCost: 80,
 	autoclickerMaxCost: 50,
-	autoclickerResults: []
+	autoclickerResults: [],
+
+	superAutoclickers: 0,
+	superAutoclickerCost: 1000,
+	superAutoclickerMin: 50,
+	superAutoclickerMax: 100,
+	superAutoclickerMinCost: 1000,
+	superAutoclickerMaxCost: 800,
+	superAutoclickerResults: []
 };
 
 // buttons
@@ -40,12 +52,24 @@ const deleteSaveBtn = document.getElementById('deleteSaveBtn');
 function updateUI() {
 	document.getElementById('numbers').innerText = numWithCommas(stats.numbers);
 	document.getElementById('primeNumbers').innerText = numWithCommas(stats.primeNumbers);
+	document.getElementById('tenNumbers').innerText = numWithCommas(stats.tenNumbers);
+
+	document.getElementById('totalNumbers').innerText = numWithCommas(stats.totalNumbers);
+	document.getElementById('totalTime').innerText = formatTime(stats.totalTime);
 	
 	document.getElementById('clickMin').innerText = numWithCommas(stats.clickMin);
 	document.getElementById('clickMax').innerText = numWithCommas(stats.clickMax);
 	document.getElementById('clickMinCost').innerText = numWithCommas(stats.clickMinCost);
 	document.getElementById('clickMaxCost').innerText = numWithCommas(stats.clickMaxCost);
-	document.getElementById('clickResults').innerText = stats.clickResults.join(", ");
+	document.getElementById('clickResults').innerHTML = stats.clickResults.map((number) => {
+		if (isPowerOfTen(number)) {
+			return `<span style="color: red;">${number}</span>`;
+		} else if (isPrime(number)) {
+			return `<span style="color: blue;">${number}</span>`;
+		} else {
+			return number;
+		}
+	}).join(", ");
 
 	document.getElementById('autoclickers').innerText = numWithCommas(stats.autoclickers);
 	document.getElementById('autoclickerCost').innerText = numWithCommas(stats.autoclickerCost);
@@ -53,19 +77,27 @@ function updateUI() {
 	document.getElementById('autoclickerMax').innerText = numWithCommas(stats.autoclickerMax);
 	document.getElementById('autoclickerMinCost').innerText = numWithCommas(stats.autoclickerMinCost);
 	document.getElementById('autoclickerMaxCost').innerText = numWithCommas(stats.autoclickerMaxCost);
-	document.getElementById('autoclickerResults').innerText = stats.autoclickerResults.join(", ");
+	document.getElementById('autoclickerResults').innerHTML = stats.autoclickerResults.map((number) => {
+		if (isPowerOfTen(number)) {
+			return `<span style="color: red;">${number}</span>`;
+		} else if (isPrime(number)) {
+			return `<span style="color: blue;">${number}</span>`;
+		} else {
+			return number;
+		}
+	}).join(", ");
 
 	document.getElementById('numbersPerSec').innerText = numWithCommas(deltaNumber);
 	document.getElementById('primeNumbersPerSec').innerText = numWithCommas(deltaPrimeNumber);
 	
 	// Hidden stuff
 	document.getElementById('unlockAutoclickers').hidden = stats.autoclickers <= 0;
-	document.getElementById('primeNumbersContainer').hidden = stats.primeNumbers <= 0;
+	//document.getElementById('primeNumbersContainer').hidden = stats.primeNumbers <= 0;
 
 	// Disable buttons
 	clickMinBtn.disabled = stats.clickMin >= stats.clickMax || stats.clickMinCost > stats.numbers;
 	clickMaxBtn.disabled = stats.clickMaxCost > stats.numbers;
-	autoclickerBuyBtn.disabled = stats.autoclickerCost > stats.numbers;
+	autoclickerBuyBtn.disabled = stats.autoclickerCost > stats.primeNumbers;
 	autoclickerMinBtn.disabled = stats.autoclickerMin >= stats.autoclickerMax || stats.autoclickerMinCost > stats.numbers || stats.autoclickers == 0;
 	autoclickerMaxBtn.disabled = stats.autoclickerMaxCost > stats.numbers || stats.autoclickers == 0;
 }
@@ -75,16 +107,23 @@ function updateUI() {
 
 clickBtn.addEventListener("click", () => {
 	let num = getRandomInt(stats.clickMin, stats.clickMax);
-	stats.numbers += num;
+
+	if (isPowerOfTen(num)) {
+		stats.tenNumbers += num;
+	} else if (isPrime(num)) {
+		stats.primeNumbers += num;
+	} else {
+		stats.numbers += num;
+	}
+
+	stats.totalNumbers += num;
 	stats.clickResults.unshift(num);
 	
 	if (stats.clickResults.length > LAST_RESULTS_LENGTH) {
 		stats.clickResults.pop();
 	}
 
-	if (isPrime(num)) {
-		stats.primeNumbers += num;
-	}
+	
 	updateUI();
 });
 
@@ -107,10 +146,10 @@ clickMaxBtn.addEventListener("click", () => {
 });
 
 autoclickerBuyBtn.addEventListener("click", () => {
-	if (stats.numbers >= stats.autoclickerCost) {
+	if (stats.primeNumbers >= stats.autoclickerCost) {
 		stats.autoclickers += 1;
-		stats.numbers -= stats.autoclickerCost;
-		stats.autoclickerCost += Math.floor(50 * Math.pow(1.1, stats.autoclickers));
+		stats.primeNumbers -= stats.autoclickerCost;
+		stats.autoclickerCost += Math.floor(100 * Math.pow(1.2, stats.autoclickers));
 	}
 	updateUI();
 });
@@ -157,18 +196,25 @@ window.setInterval(function() {
 	deltaPrimeNumber = stats.primeNumbers - lastPrimeNumber;
 	lastPrimeNumber = stats.primeNumbers;
 
+	stats.totalTime += 1;
+
 	// Autoclickers
 	for (let i = 0; i < stats.autoclickers; i++) {
 		let num = getRandomInt(stats.autoclickerMin, stats.autoclickerMax);
-		stats.numbers += num;
+
+		if (isPowerOfTen(num)) {
+			stats.tenNumbers += num;
+		} else if (isPrime(num)) {
+			stats.primeNumbers += num;
+		} else {
+			stats.numbers += num;
+		}
+
+		stats.totalNumbers += num;
 		stats.autoclickerResults.unshift(num);
 
 		if (stats.autoclickerResults.length > LAST_RESULTS_LENGTH) {
 			stats.autoclickerResults.pop();
-		}
-
-		if (isPrime(num)) {
-			stats.primeNumbers += num;
 		}
 	}
 
@@ -222,7 +268,7 @@ function getRandomInt(min, max) {
 }
 
 function numWithCommas(num) {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function isPrime(num) {
@@ -232,4 +278,23 @@ function isPrime(num) {
 		if (num % i === 0) return false;
 	}
 	return true;
+}
+
+function isPowerOfTen(num) {
+	if (num < 10) return false;
+	const log = Math.log10(num);
+	return log === Math.floor(log);
+}
+
+function formatTime(seconds) {
+	const hours = Math.floor(seconds / 3600);
+	const minutes = Math.floor((seconds % 3600) / 60);
+	const remainingSeconds = seconds % 60;
+
+	let result = "";
+	if (hours > 0) result += `${hours}hr `;
+	if (minutes > 0) result += `${minutes}m `;
+	result += `${remainingSeconds}s`;
+
+	return result.trim();
 }
